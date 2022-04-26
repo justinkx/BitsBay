4abb9b07662511e2e8f11eae8d3677a56610323e
justinkx <justinkizhakkeparampil@gmail.com>
2022-04-26 19:38:44 +0530

    update alias to babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      'module-resolver',
+      {
+        root: './',
+        alias: {
+          src: './src',
+          helpers: './src/helpers',
+          navigation: './src/navigation',
+          components: './src/components',
+          redux: './src/redux',
+          screens: './src/screens',
+        },
+      },
+    ],
+    env: {
+      production: {
+        plugins: ['transform-remove-console'],
+      },
+    },
   };
 };
