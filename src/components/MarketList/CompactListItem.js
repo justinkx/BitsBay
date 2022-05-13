86192f9cf2657621e531f8d32d80941521003172
justinkx <justinkizhakkeparampil@gmail.com>
2022-05-13 13:29:53 +0530

    added react-native-svg

--- src/components/MarketList/CompactListItem.js
+++ src/components/MarketList/CompactListItem.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native'
 import React, { memo } from 'react'
+import { Entypo } from '@expo/vector-icons'
 
-const CompactListItem = ({ item }) => {
+import { withTheme } from '../../hoc/withTheme'
+
+const CompactListItem = ({ item, layoutMode, theme }) => {
   return (
-    <View>
-      <Text>CompactListItem</Text>
+    <View style={styles.container}>
+      <View style={styles.layout1Container}>
+        <View style={styles.layout1_coin}>
+          <TouchableOpacity style={styles.starButton}>
+            <Entypo name='star' size={24} color={theme.iconActive} />
+          </TouchableOpacity>
+          <Image source={{ uri: item?.logo }} style={styles.icon} />
+        </View>
+        <View style={styles.layout1_price} />
+        <View style={styles.layout1_change} />
+      </View>
     </View>
   )
 }
 
-export default memo(CompactListItem)
+export default memo(withTheme(CompactListItem))
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  layout1Container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  layout1_coin: {},
+  layout1_price: {},
+  layout1_change: {},
+  starButton: { paddingRight: 10 },
+  icon: { width: 25, height: 25 },
+})
